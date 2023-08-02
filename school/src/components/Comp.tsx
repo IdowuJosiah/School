9d2f9ed2fe87a68e9070d243793188e30d1d8159
DanielKuteyi <dan.kuteyi@gmail.com>
2023-08-02 15:46:31 +0100

    push

--- school/src/components/Comp.tsx
+++ school/src/components/Comp.tsx
@@ -7,11 +7,14 @@ const Comp = () => {
         <div className="comp-container">
             <div className="comp-div">
                 <img alt="banner" src="./PHOTO-2023-07-17-20-25-38.jpg"/>
-                <div className="comp-text">
-                    <h2>
-                        CREATING A SOLID FOUNDATION
-                    </h2>
+                <div className="comp-overlay">
+                    <div className="comp-text">
+                        <h2>
+                            CREATING A SOLID FOUNDATION
+                        </h2>    
+                    </div>
                 </div>
+                
             </div>
             <div className="mission">
                 <div className="mission-container">
