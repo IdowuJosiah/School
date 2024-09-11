483516475f32e3d9b1adffc66a647074fb77145e
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-09-11 23:35:17 +0100

    styling and deleted node modules

--- school/src/components/Slider.tsx
+++ school/src/components/Slider.tsx
@@ -1,74 +1,64 @@
-import React, {useState, useEffect} from 'react'
-import {slideShow} from "../interfaces/interface";
-import "../css/Slider.css"
-import Button from './ButtonRegister';
+import { useState, useEffect } from "react";
+import { slideShow } from "../interfaces/interface";
+import "../css/Slider.css";
+import Button from "./ButtonRegister";
 
 const Slider = () => {
-
     const slideShowItems: slideShow[] = [
         {
-            background : "./3c15d55b-a063-431f-84d6-ced2f31f680d.JPG",
-            bannerText: "Welcome To Our School"
+            background: "./3c15d55b-a063-431f-84d6-ced2f31f680d.JPG",
+            bannerText: "Welcome To Our School",
         },
         {
-            background : "./tim-mossholder-WE_Kv_ZB1l0-unsplash.jpg",
-            bannerText: "An adventure awaits you in each class"
+            background: "./3c15d55b-a063-431f-84d6-ced2f31f680d.JPG",
+            bannerText: "An adventure awaits you in each class",
         },
         {
-            background : "./74d433cf-cebd-40d1-a999-a1e66859cba5.JPG",
-            bannerText: " Inspiring Confidence & Encouraging Self Expression"
-        }
-    ]
+            background: "./74d433cf-cebd-40d1-a999-a1e66859cba5.JPG",
+            bannerText: " Inspiring Confidence & Encouraging Self Expression",
+        },
+    ];
 
     let [index, setIndex] = useState(0);
-    let delay = 14000
+    let delay = 14000;
 
     useEffect(() => {
-        setTimeout(
-          () =>
-            setIndex((prevIndex) =>
-              prevIndex === slideShowItems.length - 1 ? 0 : prevIndex + 1
-            ),
-          delay
+        const intervalId = setInterval(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === slideShowItems.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
         );
-    
-        return () => {};
+
+        return () => clearInterval(intervalId);
         // eslint-disable-next-line
-      }, [index]);
+    }, [index]);
 
+    return (
+        <div className="slideshow">
+            <div
+                className="slideshowSlider "
+                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+                {slideShowItems.map((slide, index) => {
+                    return (
+                        <div className="slide" key={index}>
+                            <img className="slide-image" src={slide.background} alt="#" />
 
-  return (
-    <div className="slideshow">
-      <div
-       className="slideshowSlider "
-       style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-       >
-        {
-            slideShowItems.map((slide, index)=> {
-                return  <div className="slide" key={index}>
-                        
-                          <img className='slide-image' src={slide.background} alt='#'/>
-                          
-                          <div className="slide-overlay">
-                           
-                              <div className='slide-banner'>
-                                      
-                                    
-                                     <div className='slide-text'>{slide.bannerText}</div>
-                                     
-                                  
-                                    <Button/>
-                              </div>
-                            
-                            
-                          </div>
-                </div>
-            })
-        }
-      </div>
-      
-    </div>
-  )
-}
+                            <div className="slide-overlay">
+                                <div className="slide-banner">
+                                    <div className="slide-text">{slide.bannerText}</div>
 
-export default Slider
+                                    <Button />
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
